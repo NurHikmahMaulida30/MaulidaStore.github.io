9fd4f89e8e53a12faa210ad6a8589b27b0861606
NurHikmahMaulida30 <nurhikmahmaulid30@gmail.com>
2021-08-02 10:58:33 +0700

    first commit

---
+++ src/components/WomenBaner.vue
@@ -0,0 +1,122 @@
+<template>
+    <div>
+        <!-- Women Banner Section Begin (Item) -->
+    <section class="women-banner spad">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12 mt-5">
+                    <carousel class="product-slider" :items="3" :dot="false" :nav="false" :autoplay="true">
+                        <div class="product-item">
+                            <div class="pi-pic">
+                                <img src="img/buah1.jpg" alt="" />
+                                <ul>
+                                    <li class="w-icon active">
+                                        <a href="#"><i class="icon_bag_alt"></i></a>
+                                    </li>
+                                    <li class="quick-view">
+                                  <router-link to="/Product"> Quick View
+                                  </router-link ></li>
+                                </ul>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">Coat</div>
+                                <router-link to="/Product"><a href="#">
+                                    <h5>Paprika</h5>
+                                </a>
+                                </router-link >
+                                <div class="product-price">
+                                    $14.00
+                                    <span>$35.00</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="product-item">
+                            <div class="pi-pic">
+                                <img src="img/buah2.jpg" alt="" />
+                                <ul>
+                                    <li class="w-icon active">
+                                        <a href="#"><i class="icon_bag_alt"></i></a>
+                                    </li>
+                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
+                                </ul>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">Shoes</div>
+                                <a href="#">
+                                    <h5>Guangzhou sweater</h5>
+                                </a>
+                                <div class="product-price">
+                                    $13.00
+                                </div>
+                            </div>
+                        </div>
+                        <div class="product-item">
+                            <div class="pi-pic">
+                                <img src="img/buah3.jpg" alt="" />
+                                <ul>
+                                    <li class="w-icon active">
+                                        <a href="#"><i class="icon_bag_alt"></i></a>
+                                    </li>
+                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
+                                </ul>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">Towel</div>
+                                <a href="#">
+                                    <h5>Pure Pineapple</h5>
+                                </a>
+                                <div class="product-price">
+                                    $34.00
+                                </div>
+                            </div>
+                        </div>
+                        <div class="product-item">
+                            <div class="pi-pic">
+                                <img src="img/buah4.jpg" alt="" />
+                                <ul>
+                                    <li class="w-icon active">
+                                        <a href="#"><i class="icon_bag_alt"></i></a>
+                                    </li>
+                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
+                                    <li class="w-icon">
+                                        <a href="#"><i class="fa fa-random"></i></a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">Towel</div>
+                                <a href="#">
+                                    <h5>Converse Shoes</h5>
+                                </a>
+                                <div class="product-price">
+                                    $34.00
+                                </div>
+                            </div>
+                        </div>
+                    </carousel>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Women Banner Section End -->
+
+    </div>
+</template>
+
+<script>
+import carousel from 'vue-owl-carousel'
+
+export default {
+    nama: 'WomenBaner',
+   components: {
+     carousel
+  }
+}
+</script>
+
+<style scoped>
+.product-item{
+    margin-right: 30px;
+}
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+     <HeaderMaulida/>
+     <HeroSection/>
+     <WomenBaner/>
+     <InstaStore/>
+     <LogoStore/>
+     <FooterSelection/>
+      <RelatedProducts/>
+       </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderMaulida from '@/components/HeaderMaulida.vue'
+import HeroSection from '@/components/HeroSection.vue'
+import WomenBaner from '@/components/WomenBaner.vue'
+import InstaStore from '@/components/InstaStore.vue'
+import LogoStore from '@/components/LogoStore.vue'
+import FooterSelection from '@/components/FooterSelection.vue'
+
+
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderMaulida,
+    HeroSection,
+    WomenBaner,
+    InstaStore,
+    LogoStore,
+   FooterSelection
+   
+  }
+}
+</script>
